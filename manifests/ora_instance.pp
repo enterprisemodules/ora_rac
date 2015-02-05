e8f65e30f69fe7453526ce7f9e4ff0b489a5fb33
Bert Hajee <hajee@moretIA.com>
2015-02-05 12:05:20 +0100

    Only enable log's when not already done

--- manifests/ora_instance.pp
+++ manifests/ora_instance.pp
@@ -77,6 +77,7 @@ define ora_rac::ora_instance(
   }
 
   ora_exec{"@/tmp/add_logfiles_${thread}.sql@${on}":
+    unless    => "select * from v\$log where THREAD#=${thread}",
     require   => [
       File["/tmp/add_logfiles_${thread}.sql"],
     ]
