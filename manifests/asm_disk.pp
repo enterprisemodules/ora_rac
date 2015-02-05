d07e4e69c8c77d6c491f8e3e9f0286308ef72424
Bert Hajee <hajee@moretIA.com>
2015-02-05 11:58:29 +0100

    Add support for partitions without begin and end
    Adding partitions without a specified begin and end, result in a partition using the full size of the disk.

--- manifests/asm_disk.pp
+++ manifests/asm_disk.pp
@@ -3,9 +3,11 @@
 # === Parameters
 #
 # name      partition name if the form /dev/sda:1
-# name    asm name name
-# start     start of the partition
-# end       end of the partition
+# name      asm name name
+# start     start of the partition. If not specified or undef, it will default to the
+#           first available sector on the disk.
+# end       end of the partition. If not specfied or undef, it will default to the last
+#           available block on the disk. Therefore filling the whole disk.
 #
 # === Variables
 #
@@ -16,17 +18,16 @@
 #
 define ora_rac::asm_disk(
   $raw_device,
-  $start    = '0%'  ,
-  $end      = '100%',
+  $start    = undef,
+  $end      = undef,
 )
 {
   #
   # Validation
   #
-  assert_type(String[1],$raw_device)      |$e, $a| { fail "raw_device is ${a}, but must be a non empty string"}
-  validate_re($raw_device, '.*\:.*')    # You must specify a device with a partition number
-  validate_re($start,'\d.*\S?[%|k|K|m|M|g|G][b|B]?')
-  validate_re($end,'\d.*\S?[%|k|K|m|M|g|G][b|B]?')
+  assert_type(Regexp[/.*\:.*/], $raw_device) |$e, $a| { fail "raw_device is ${a}, but you must specify a device with a partition number"}
+  assert_type(Optional[Regexp[/\d.*\S?[%|k|K|m|M|g|G][b|B]?/]],$start )  |$e, $a| { fail "start is ${a}, but you must be either undef or a valid size string"}
+  assert_type(Optional[Regexp[/\d.*\S?[%|k|K|m|M|g|G][b|B]?/]],$end ) |$e, $a| { fail "end is ${a}, but you must be either undef or a valid size string"}
 
   #
   # Manipulation and translation of parameters
@@ -35,10 +36,6 @@ define ora_rac::asm_disk(
   $device_name      = $_device_array[0]
   $partition_number = $_device_array[1]
   $device           = "${device_name}${partition_number}"
-
-  #
-  #
-  #
 
   partition_table{$device_name:
     ensure  => 'gpt',
