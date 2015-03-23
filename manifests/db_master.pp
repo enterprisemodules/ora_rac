228b0c309b9ece9815a82b6f65d511ac7a52ecc1
Bert Hajee <hajee@moretIA.com>
2015-03-23 10:28:14 +0100

    Small fix to db_master

--- manifests/db_master.pp
+++ manifests/db_master.pp
@@ -171,7 +171,7 @@ class ora_rac::db_master(
 
   exec{'register_diskgroups':
     user          => $ora_rac::settings::oracle_user,
-    environment   => ["ORACLE_SID=${current_instance}", "ORAENV_ASK=NO", "ORACLE_HOME=${ora_rac::settings::oracle_home}"],
+    environment   => ["ORACLE_SID=${db_name}1", "ORAENV_ASK=NO", "ORACLE_HOME=${ora_rac::settings::oracle_home}"],
     command       => "${ora_rac::settings::oracle_home}/bin/srvctl modify database -d ${db_name} -a ${ora_rac::settings::disk_group_names.join(',')}",
     logoutput     => on_failure,
   }
